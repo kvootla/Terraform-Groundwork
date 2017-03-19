11268bed66bdb3cf2d8c32993d2a4bf6f7a1677d
Krishna Vootla <vootla.krishna@gmail.com>
2017-03-19 10:39:49 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+provider "aws" {
+    access_key = "${var.aws_access_key}"
+    secret_key = "${var.aws_secret_key}"
+    region = "${var.aws_region}"
+}
+
 resource "aws_vpc" "mod" {
   cidr_block           = "${var.cidr}"
   enable_dns_hostnames = "${var.enable_dns_hostnames}"
