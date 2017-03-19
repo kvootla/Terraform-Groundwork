9ead04e7709cd926bd5392900934fea148e96c63
Krishna Vootla <vootla.krishna@gmail.com>
2017-03-19 10:02:30 -0400

    commit 2

--- main.tf
+++ main.tf
@@ -1,4 +1,6 @@
 resource "aws_vpc" "mod" {
   cidr_block           = "${var.cidr}"
+  enable_dns_hostnames = "${var.enable_dns_hostnames}"
+  enable_dns_support   = "${var.enable_dns_support}"
   name                 = "${var.name}"
-}
+  }
