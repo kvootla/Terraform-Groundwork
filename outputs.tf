4b4d105248c2e95247e3aa9521e9e892a7c98aad
Krishna Vootla <vootla.krishna@gmail.com>
2017-03-19 11:16:13 -0400

    commit

--- outputs.tf
+++ outputs.tf
@@ -1,39 +1,3 @@
-output "flow_log_cloudwatch_log_group_arn" {
-  value = "${aws_cloudwatch_log_group.cloudwatch_log_group.arn}"
-}
-
-#output "flow_log_cloudwatch_log_group_name" {
-#  value = "${aws_cloudwatch_log_group.cloudwatch_log_group.name}"
-#}
-
-output "flow_log_cloudwatch_log_stream_arn" {
-  value = "${aws_cloudwatch_log_stream.cloudwatch_log_stream.arn}"
-}
-
-output "internet_gateway_id" {
-  value = "${aws_internet_gateway.internet_gateway.id}"
-}
-
-output "nat_eips" {
-  value = ["${aws_eip.eip.*.id}"]
-}
-
-output "private_route_table_ids" {
-  value = ["${aws_route_table.private_route_table.*.id}"]
-}
-
-output "private_subnet_ids" {
-  value = ["${aws_subnet.private_subnet.*.id}"]
-}
-
-output "private_subnet_cidr_blocks" {
-  value = ["${aws_subnet.private_subnet.*.cidr_block}"]
-}
-
-output "public_route_table_ids" {
-  value = ["${aws_route_table.public_route_table.*.id}"]
-}
-
 output "public_subnet_cidr_blocks" {
   value = ["${aws_subnet.public_subnet.*.cidr_block}"]
 }
