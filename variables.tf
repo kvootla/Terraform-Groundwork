c8cd63d238a4a709ef02da03c20995bb26fa3ed6
Krishna Vootla <vootla.krishna@gmail.com>
2017-03-19 10:16:02 -0400

    commit 2

--- variables.tf
+++ variables.tf
@@ -2,6 +2,12 @@ variable "name" {}
 
 variable "cidr" {}
 
+variable "aws_access_key" {}
+
+variable "aws_secret_key" {}
+
+variable "aws_region" {}
+
 variable "enable_dns_hostnames" {
   description = "should be true if you want to use private DNS within the VPC"
   default     = false
