41d11d64dbc13b1c6a9f75eea0f0b6cfb0a12f97
Krishna Vootla <vootla.krishna@gmail.com>
2017-03-19 11:41:44 -0400

    commit 6

--- main.tf
+++ main.tf
@@ -19,8 +19,9 @@ provider "aws" {
 
 // Create Public Subnet
 resource "aws_subnet" "public_subnet" {
+  vpc_id                  = "${aws_vpc.vpc.id}"
   vpc_name                = "${var.vpc_name}"
   cidr_block              = "${var.public_subnets}"
-  availability_zones       = "${var.availability_zones}"
+  availability_zone       = "${var.availability_zone}"
   map_public_ip_on_launch = "${var.map_public_ip_on_launch}"
 }
