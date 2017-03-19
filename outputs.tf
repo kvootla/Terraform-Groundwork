f0265524665a11fac1b682566e62cff39b1094e4
Krishna Vootla <vootla.krishna@gmail.com>
2017-03-19 11:18:13 -0400

    commit 3

--- outputs.tf
+++ outputs.tf
@@ -6,11 +6,6 @@ output "public_subnet_ids" {
   value = ["${aws_subnet.public_subnet.*.id}"]
 }
 
-output "vpc_cidr_block" {
-  value = "${aws_vpc.vpc.cidr_block}"
+output "vpc_cidr" {
+  value = "${aws_vpc.vpc.cidr}"
 }
-
-output "vpc_id" {
-  value = "${aws_vpc.vpc.id}"
-}
-
