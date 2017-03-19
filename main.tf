6d4d0a46ce1bf755eb9587202298505f77947d17
Krishna Vootla <vootla.krishna@gmail.com>
2017-03-19 10:15:11 -0400

    commit 2

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+provider "aws" {
+    access_key = "${var.aws_access_key}"
+    secret_key = "${var.aws_secret_key}"
+}
+
 resource "aws_vpc" "mod" {
   cidr_block           = "${var.cidr}"
   enable_dns_hostnames = "${var.enable_dns_hostnames}"
