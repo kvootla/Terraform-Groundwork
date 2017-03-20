0c9bb68f50e582bfe0c2eacda1ea67780867edc1
Krishna Vootla <vootla.krishna@gmail.com>
2017-03-20 11:53:53 -0400

    commit 234

--- main.tf
+++ main.tf
@@ -1,21 +1,17 @@
 // Create Private Subnet
-//resource "aws_subnet" "private_subnet" {
-  //vpc_id                  = "${aws_vpc.vpc.id}"
-  //cidr_block              = "${var.private_subnets[count.index]}"
-  //availability_zone       = "${var.availability_zones[count.index]}"
-  //count                   = "${length(var.private_subnets)}"
-  //map_public_ip_on_launch = false
-
-  //tags {
-    //Name = "${var.vpc_name}-private-subnet-${element(var.availability_zones, count.index)}"
-  //}
-//}
+resource "aws_subnet" "private_subnet" {
+  vpc_id                  = "${aws_vpc.vpc.id}"
+  cidr_block              = "${var.private_subnets[count.index]}"
+  availability_zone       = "${var.availability_zones[count.index]}"
+  map_public_ip_on_launch = false
+  }
 
 provider "aws" {
     access_key = "${var.aws_access_key}"
     secret_key = "${var.aws_secret_key}"
     region = "${var.aws_region}"
 }
+
 
 // Create Public Subnet
 resource "aws_subnet" "public_subnet" {
