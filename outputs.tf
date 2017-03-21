81766ebae103466b9037d01fb25cf49cb49e119b
Krishna Vootla <vootla.krishna@gmail.com>
2017-03-21 15:17:51 -0400

    commit 321

--- outputs.tf
+++ outputs.tf
@@ -1,24 +1,24 @@
 # this approach is not working just yet
 output "arn" {
-  value = "${coalesce(aws_alb.alb_loging.arn, aws_alb.alb_nologing.arn) }"
+  value = "${coalesce(var.alb_loging.arn, var.alb_nologing.arn) }"
 
-  /*value = "${aws_alb.alb_loging.arn}"*/
+  /*value = "${var.alb_loging.arn}"*/
 }
 
 output "dns_name" {
-  value = "${aws_alb.alb_loging.dns_name}"
+  value = "${var.alb_loging.dns_name}"
 
-  /*value = "${coalesce(list("aws_alb.alb_loging.dns_name", "aws_alb.alb_nologing.dns_name")) }"*/
+  /*value = "${coalesce(list("var.alb_loging.dns_name", "var.alb_nologing.dns_name")) }"*/
 }
 
 output "id" {
-  value = "${aws_alb.alb_loging.id}"
+  value = "${var.alb_loging.id}"
 
-  /*value = "${coalesce(list("aws_alb.alb_loging.dns_name", "aws_alb.alb_nologing.dns_name")) }"*/
+  /*value = "${coalesce(list("var.alb_loging.dns_name", "var.alb_nologing.dns_name")) }"*/
 }
 
 output "zone_id" {
-  value = "${aws_alb.alb_loging.zone_id}"
+  value = "${var.alb_loging.zone_id}"
 
-  /*value = "${coalesce(list("aws_alb.alb_loging.dns_name", "aws_alb.alb_nologing.dns_name")) }"*/
+  /*value = "${coalesce(list("var.alb_loging.dns_name", "var.alb_nologing.dns_name")) }"*/
 }
