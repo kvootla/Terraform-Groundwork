3a9b77303be5d707d3c985383632ab40919997bc
Krishna Vootla <vootla.krishna@gmail.com>
2017-03-21 11:42:42 -0400

    commit 432

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+provider "aws" {
+    access_key = "${var.aws_access_key}"
+    secret_key = "${var.aws_secret_key}"
+    region = "${var.aws_region}"
+}
+
 # Assign AWS EIP to AWS EC2
 resource "aws_eip" "eip" {
   instance = "${element(split(",", var.instance_ids), count.index)}"
