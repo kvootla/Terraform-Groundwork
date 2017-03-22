7ef198468a8cc88d7db559750dabb8cc8c0c9a59
Krishna Vootla <vootla.krishna@gmail.com>
2017-03-22 10:28:20 -0400

    commit 432

--- main.tf
+++ main.tf
@@ -16,8 +16,6 @@ resource "aws_alb" "alb_loging" {
     bucket = "${var.log_bucket}"
     prefix = "${var.log_prefix}"
   }
-
-  count = "${var.log_bucket != "" && var.log_prefix != "" ? 1 : 0}"
 }
 
 resource "aws_alb" "alb_nologing" {
@@ -25,6 +23,4 @@ resource "aws_alb" "alb_nologing" {
   subnets         = "${var.subnets}"
   security_groups = "${var.alb_security_groups}"
   internal        = "${var.alb_is_internal}"
-
-  count = "${(var.log_bucket == "" || var.log_prefix == "") ? 1 : 0}"
 }
