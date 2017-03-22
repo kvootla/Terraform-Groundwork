5e205f05d76865be5f42ba958cdeabdae0987afe
Krishna Vootla <vootla.krishna@gmail.com>
2017-03-22 10:44:07 -0400

    commit 233

--- main.tf
+++ main.tf
@@ -8,19 +8,23 @@ provider "aws" {
 
 resource "aws_alb" "alb_loging" {
   name            = "${var.alb_name}"
-  subnets         = "${var.subnets}"
-  security_groups = "${var.alb_security_groups}"
+  subnets         = ["${split(",", var.subnets)}"]
+  security_groups = ["${split(",", var.alb_security_groups)}"]
   internal        = "${var.alb_is_internal}"
 
   access_logs {
     bucket = "${var.log_bucket}"
     prefix = "${var.log_prefix}"
   }
+
+  count = "${var.log_bucket != "" && var.log_prefix != "" ? 1 : 0}"
 }
 
 resource "aws_alb" "alb_nologing" {
   name            = "${var.alb_name}"
-  subnets         = "${var.subnets}"
-  security_groups = "${var.alb_security_groups}"
+  subnets         = ["${split(",", var.subnets)}"]
+  security_groups = ["${split(",", var.alb_security_groups)}"]
   internal        = "${var.alb_is_internal}"
+
+  count = "${(var.log_bucket == "" || var.log_prefix == "") ? 1 : 0}"
 }
