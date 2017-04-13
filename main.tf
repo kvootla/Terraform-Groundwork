e22637bc2875f5427b966d0d3ccb26359b92c76d
Krishna Vootla <vootla.krishna@gmail.com>
2017-04-13 10:15:37 -0400

    commit 32334

--- main.tf
+++ main.tf
@@ -1,3 +1,18 @@
+/**
+ * Input Variables
+ */
+
+variable "vpc" { default = true }
+variable "instance_ids" { default = "" }
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "aws_region" {}
+
+
+/**
+ * Resource for Elastic IP
+ */
+
 provider "aws" {
     access_key = "${var.aws_access_key}"
     secret_key = "${var.aws_secret_key}"
@@ -10,3 +25,10 @@ resource "aws_eip" "eip" {
   vpc      = "${var.vpc}"
   count    = "${length(split(",", var.instance_ids))}"
 }
+
+
+/**
+ * Outputs Varibales
+ */
+
+output "public_ip" { value = "${aws_eip.eip.public_ip}" }
