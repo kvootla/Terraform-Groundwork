ae3b3de7d683d5c20a12b1e9df619b0ee65555e4
Krishna Vootla <vootla.krishna@gmail.com>
2017-04-13 10:23:27 -0400

    commit 21332

--- main.tf
+++ main.tf
@@ -1,3 +1,19 @@
+/**
+ * Input Variables
+ */
+
+variable "name" { default = "igw" }
+variable "vpc_id" {}
+
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "aws_region" {}
+
+
+/**
+ * Resource for Internet Gateway
+ */
+
 provider "aws" {
     access_key = "${var.aws_access_key}"
     secret_key = "${var.aws_secret_key}"
@@ -8,3 +24,10 @@ resource "aws_internet_gateway" "igw" {
   vpc_id = "${var.vpc_id}"
      tags { Name = "${var.name}" }
 }
+
+
+/**
+ * Outputs Varibales
+ */
+
+output "igw_id" { value = "${aws_internet_gateway.igw.id}" }
