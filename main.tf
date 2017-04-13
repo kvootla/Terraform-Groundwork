a45aee2c5bea0daa6e74712c40abc4a2c3794588
Krishna Vootla <vootla.krishna@gmail.com>
2017-04-13 10:21:30 -0400

    commit 34231

--- main.tf
+++ main.tf
@@ -1,12 +1,114 @@
+/**
+ * Input Variables
+ */
+
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "aws_region" {}
+
+# RDS Instance Variables
+
+variable "rds_instance_identifier" {
+    description = "Custom name of the instance"
+}
+
+variable "rds_is_multi_az" {
+    description = "Set to true on production"
+    default = false
+}
+
+variable "rds_storage_type" {
+    description = "One of 'standard' (magnetic), 'gp2' (general purpose SSD), or 'io1' (provisioned IOPS SSD)."
+    default = "standard"
+}
+
+variable "rds_allocated_storage" {
+    description = "The allocated storage in GBs"
+    # You just give it the number, e.g. 10
+}
+
+variable "rds_engine_type" {
+    description = "Database engine type"
+    # Valid types are
+    # - mysql
+    # - postgres
+    # - oracle-*
+    # - sqlserver-*
+    # See http://docs.aws.amazon.com/cli/latest/reference/rds/create-db-instance.html
+    # --engine
+}
+
+variable "rds_engine_version" {
+    description = "Database engine version, depends on engine type"
+    # For valid engine versions, see:
+    # See http://docs.aws.amazon.com/cli/latest/reference/rds/create-db-instance.html
+    # --engine-version
+}
+
+variable "rds_instance_class" {
+    description = "Class of RDS instance"
+    # Valid values
+    # https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/Concepts.DBInstanceClass.html
+}
+
+variable "auto_minor_version_upgrade" {
+    description = "Allow automated minor version upgrade"
+    default = true
+}
+
+variable "allow_major_version_upgrade" {
+    description = "Allow major version upgrade"
+    default = false
+}
+
+variable "database_name" {
+    description = "The name of the database to create"
+}
+
+# Self-explainatory variables
+variable "database_user" {}
+variable "database_password" {}
+variable "database_port" {}
+
+# This is for a custom parameter to be passed to the DB
+# We're "cloning" default ones, but we need to specify which should be copied
+variable "db_parameter_group" {
+    description = "Parameter group, depends on DB engine used"
+    # default = "mysql5.6"
+    # default = "postgres9.5"
+}
+
+variable "publicly_accessible" {
+    description = "Determines if database can be publicly available (NOT recommended)"
+    default = false
+}
+
+# RDS Subnet Group Variables
+variable "subnets" {
+    description = "List of subnets DB should be available at. It might be one subnet."
+    type = "list"
+}
+
+variable "private_cidr" {
+    description = "VPC private addressing, used for a security group"
+    type = "string"
+}
+
+variable "rds_vpc_id" {
+    description = "VPC to connect to, used for a security group"
+    type = "string"
+}
+
+
+/**
+ * Resource for RDS
+ */
+
 provider "aws" {
     access_key = "${var.aws_access_key}"
     secret_key = "${var.aws_secret_key}"
     region = "${var.aws_region}"
 }
-
-//
-// Module: tf_aws_rds
-//
 
 // This template creates the following resources
 // - An RDS instance
@@ -65,9 +167,9 @@ resource "aws_db_subnet_group" "main_db_subnet_group" {
 
 # Security groups
 resource "aws_security_group" "main_db_access" {
-  name = "Database access"
-  description = "Allow access to the database"
-  vpc_id = "${var.rds_vpc_id}"
+    name = "Database access"
+    description = "Allow access to the database"
+    vpc_id = "${var.rds_vpc_id}"
 }
 
 resource "aws_security_group_rule" "allow_db_access" {
@@ -90,4 +192,34 @@ resource "aws_security_group_rule" "allow_all_outbound" {
     cidr_blocks = ["0.0.0.0/0"]
 
     security_group_id = "${aws_security_group.main_db_access.id}"
+}
+
+
+/**
+ * Outputs Varibales
+ */
+
+# Output the ID of the RDS instance
+output "rds_instance_id" {
+    value = "${aws_db_instance.main_rds_instance.id}"
+}
+
+# Output the address (aka hostname) of the RDS instance
+output "rds_instance_address" {
+    value = "${aws_db_instance.main_rds_instance.address}"
+}
+
+# Output endpoint (hostname:port) of the RDS instance
+output "rds_instance_endpoint" {
+    value = "${aws_db_instance.main_rds_instance.endpoint}"
+}
+
+# Output the ID of the Subnet Group
+output "subnet_group_id" {
+    value = "${aws_db_subnet_group.main_db_subnet_group.id}"
+}
+
+# Output DB security group ID
+output "security_group_id" {
+    value = "${aws_security_group.main_db_access.id}"
 }
