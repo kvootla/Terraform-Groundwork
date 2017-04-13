de2b4d981021b54d19bc4e531d7609edb3898f9a
Krishna Vootla <vootla.krishna@gmail.com>
2017-04-13 10:29:33 -0400

    commit 24434

--- elb_https/main.tf
+++ elb_https/main.tf
@@ -1,19 +1,66 @@
-//
-// Module: tf_aws_elb/elb_https
-//
+/**
+ * Input Variables
+ */
 
-// Provider specific configs
+variable "elb_name" {}
+
+variable "elb_is_internal" {
+  description = "Determines if the ELB is internal or not"
+  default = false
+  // Defaults to false, which results in an external IP for the ELB
+}
+
+variable "elb_security_group" {}
+
+// See README.md for details on finding the
+// ARN of an SSL certificate in EC2
+variable "ssl_certificate_id" {
+  description = "The ARN of the SSL Certificate in EC2"
+}
+
+variable "subnet_az1" {
+  description = "The subnet for AZ1"
+}
+
+variable "subnet_az2" {
+  description = "The subnet for AZ2"
+}
+
+variable "backend_port" {
+  description = "The port the service on the EC2 instances listens on"
+}
+
+variable "backend_protocol" {
+ description = "The protocol the backend service speaks"
+ // Possible options are
+ // - http
+ // - https
+ // - tcp
+ // - ssl (secure tcp)
+}
+
+variable "health_check_target" {
+  description = "The URL the ELB should use for health checks"
+  // This is primarily used with `http` or `https` backend protocols
+  // The format is like `HTTPS:443/health`
+}
+
+// Variables for providers used in this module
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "aws_region" {}
+
+
+/**
+ * Resource for ELB/https
+ */
+
 provider "aws" {
     access_key = "${var.aws_access_key}"
     secret_key = "${var.aws_secret_key}"
     region = "${var.aws_region}"
 }
 
-// ELB Resource for Module
-// A note about instances:
-// - This module assumes your instances will be made
-//   by an ASG and the ASG will associate them with
-//   the ELB.
 resource "aws_elb" "elb" {
   name = "${var.elb_name}"
   subnets = ["${var.subnet_az1}","${var.subnet_az2}"]
@@ -37,4 +84,21 @@ resource "aws_elb" "elb" {
   }
 
   cross_zone_load_balancing = true
+}
+
+
+/**
+ * Outputs Varibales
+ */
+
+output "elb_id" {
+  value = "${aws_elb.elb.id}"
+}
+
+output "elb_name" {
+  value = "${aws_elb.elb.name}"
+}
+
+output "elb_dns_name" {
+  value = "${aws_elb.elb.dns_name}"
 }
