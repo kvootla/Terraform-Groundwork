2c32f1a0ef60c1b015a01ffe0c62ec568a1b0d1a
Krishna Vootla <vootla.krishna@gmail.com>
2017-04-16 01:22:19 -0400

    331441

--- main.tf
+++ main.tf
@@ -2,6 +2,10 @@
  * Inputs
  */
 
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "aws_region" {}
+
 variable "cidrs" {
   type        = "map"
   description = "A map with key being the availability zone and value the CIDR range."
@@ -21,9 +25,16 @@ variable "organization" {}
 
 variable "environment" {}
 
+
 /**
  * Subnets
  */
+
+provider "aws" {
+    access_key = "${var.aws_access_key}"
+    secret_key = "${var.aws_secret_key}"
+    region = "${var.aws_region}"
+}
 
 resource "aws_subnet" "main" {
   vpc_id                  = "${var.vpc_id}"
@@ -82,6 +93,11 @@ resource "aws_route" "igw" {
     create_before_destroy = true
   }
 }
+
+
+/**
+ * Outputs
+ */
 
 output "subnet_ids" {
   value = [
