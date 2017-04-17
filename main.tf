7f4f37a8cf3c6271ed5550d0875233d6809a7350
Krishna Vootla <vootla.krishna@gmail.com>
2017-04-17 19:24:27 -0400

    commit 43525

--- main.tf
+++ main.tf
@@ -38,6 +38,11 @@ variable "aws_secret_key" {}
 variable "aws_region" {}
 
 
+variable "security_group_id" {
+  description = "The name for the security group"
+}
+
+
 /**
  * EC2 Instance
  */
@@ -45,17 +50,18 @@ variable "aws_region" {}
 provider "aws" {
     access_key = "${var.aws_access_key}"
     secret_key = "${var.aws_secret_key}"
-    region = "${var.aws_region}"
+    region     = "${var.aws_region}"
 }
 
 resource "aws_instance" "ec2_instance" {
-    ami 			 = "${var.ami_id}"
-    count 			 = "${var.number_of_instances}"
-    subnet_id 		 = "${var.subnet_id}"
+    ami 			       = "${var.ami_id}"
+    count 			     = "${var.number_of_instances}"
+    subnet_id 		   = "${var.subnet_id}"
     instance_type    = "${var.instance_type}"
-    user_data 		 = "${file(var.user_data)}"
-    key_name 		 = "${var.key_name}"
-    
+    user_data 		   = "${file(var.user_data)}"
+    key_name 		     = "${var.key_name}"
+    security_group   = "${var.security_group_id}"  
+  
     tags {
         created_by = "${lookup(var.tags,"created_by")}"
         // Takes the instance_name input variable and adds
@@ -64,6 +70,7 @@ resource "aws_instance" "ec2_instance" {
         Name = "${var.instance_name}-${count.index}"
     }
 }
+
 
 
 /**
