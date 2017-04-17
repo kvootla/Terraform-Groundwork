e2633f83ca398fb734c56dc358017caea65d358d
Krishna Vootla <vootla.krishna@gmail.com>
2017-04-17 14:44:24 -0400

    commit 34241

--- main.tf
+++ main.tf
@@ -1,41 +1,35 @@
 /**
- * Inputs
+ * Input Variables
  */
-  
-variable "ami_id" {
-  description = "AMI to serve as base of server build"
-}
 
-variable "instance_type" {
-  description = "Instance type"
-}
 variable "instance_name" {
-  description = "Used to populate the Name tag"
+  description = "Used to populate the Name tag. This is done in main.tf"
 }
 
-variable "number_of_instances" {
-  description = "number of instances"
-  default = 1
-}
-
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-variable "aws_region" {}
-
+variable "instance_type" {}
 
 variable "subnet_id" {
   description = "The VPC subnet the instance(s) will go in"
 }
 
+variable "key_name" {}
+
 variable "vpc_id" {
   description = "VPC ID"
+}
+
+variable "ami_id" {
+  description = "The AMI to use"
+}
+
+variable "number_of_instances" {
+  description = "number of instances to make"
+  default = 1
 }
 
 variable "user_data" {
    description = "The path to a file with user_data for the instances"
 }
-
-variable "key_name" {}
 
 variable "tags" {
   default = {
@@ -43,39 +37,44 @@ variable "tags" {
  }
 }
 
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "aws_region" {}
+
 
 /**
- * EC2 Instances
+ * EC2 Instance
  */
 
 provider "aws" {
     access_key = "${var.aws_access_key}"
     secret_key = "${var.aws_secret_key}"
-    region     = "${var.aws_region}"
+    region = "${var.aws_region}"
 }
 
-resource "aws_instance" "main" {
-    ami		         = "${var.ami_id}"
+resource "aws_instance" "ec2_instance" {
+    ami 			 = "${var.ami_id}"
+    count 			 = "${var.number_of_instances}"
+    subnet_id 		 = "${var.subnet_id}"
     instance_type    = "${var.instance_type}"
-    instance_name    = "${var.instance_name}"
-    count	         = "${var.number_of_instances}"
-    subnet_id	     = "${var.subnet_id}"
     vpc_id           = "${var.vpc_id}"
-    user_data	     = "${file(var.user_data)}"
-    key_name 	     = "${var.key_name}"
+    user_data 		 = "${file(var.user_data)}"
+    key_name 		 = "${var.key_name}"
     
-
     tags {
         created_by = "${lookup(var.tags,"created_by")}"
-        Name	     = "${var.instance_name}-${count.index}"
+        // Takes the instance_name input variable and adds
+        //  the count.index to the name., e.g.
+        //  "example-host-web-1"
+        Name = "${var.instance_name}-${count.index}"
     }
 }
 
 
 /**
- * Outputs
+ * Outputs Varibales
  */
 
 output "ec2_instance_id" {
-  value = "${aws_instance.main.id}"
+  value = "${aws_instance.ec2_instance.id}"
 }
