8e604185e2e5b6bcee123bf402d6d98758d6443b
Krishna Vootla <vootla.krishna@gmail.com>
2017-04-17 09:04:35 -0400

    commit 23224

--- main.tf
+++ main.tf
@@ -2,31 +2,12 @@
  * Input Variables
  */
 
-variable "cidrs" {}
+variable "vpc_cidr" {}
 
 variable "vpc_id" {}
 
 variable "availability_zones" {
   description = "A list of availability zones inside the VPC"
-}
-
-variable "enable_dns_hostnames" {
-  description = "True to use private DNS within the VPC"
-  default     = true
-}
-
-variable "enable_dns_support" {
-  description = "True to use private DNS within the VPC"
-  default     = true
-}
-
-variable "private_propagating_vgws" {
-  description = "A list of VGWs the private route table should propagate."
-  default     = []
-}
-
-variable "private_subnets" {
-  default = "A list of public subnets inside the VPC"
 }
 
 variable "map_public_ip_on_launch" {
@@ -43,13 +24,18 @@ variable "public_subnets" {
   description = "A list of public subnets inside the VPC"
 }
 
-variable "organization" {}
-
-variable "environment" {}
-
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
 variable "aws_region" {}
+
+variable "organization" {
+  description = "Organization the VPC is for."
+}
+
+variable "environment" {
+  description = "Environment the VPC is for."
+  default     = ""
+}
 
   
 /**
@@ -64,21 +50,16 @@ provider "aws" {
 
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = "${var.vpc_id}"
-  cidrs                   = "${var.public_subnets}"
+  cidr_block              = "${var.public_subnets}"
   availability_zone       = "${var.availability_zones}"
   map_public_ip_on_launch = "${var.map_public_ip_on_launch}"
+}
 
-  lifecycle {
-    create_before_destroy = true
-  }
-
-
-  tags {
+tags {
     Name         = "${var.environment == "" ? var.organization : format("%s-%s", var.organization, var.environment)}-subnet"
     Organization = "${var.organization}"
     Terraform    = "true"
   }
-}
 
 
 /**
@@ -86,8 +67,8 @@ resource "aws_subnet" "public_subnet" {
  */
 
 
-output "public_subnet_cidrs" {
-  value = ["${aws_subnet.public_subnet.*.cidrs}"]
+output "public_subnet_cidr_blocks" {
+  value = ["${aws_subnet.public_subnet.*.cidr_block}"]
 }
 
 output "public_subnet_ids" {
