4d8b6f7f5a966a0c69e8d818a51464c028d6cea1
Krishna Vootla <vootla.krishna@gmail.com>
2017-04-17 19:45:43 -0400

    commit 32411

--- main.tf
+++ main.tf
@@ -17,6 +17,10 @@ variable "map_public_ip_on_launch" {
   default = false
 }
 
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "aws_region" {}
+
 variable "organization" {}
 
 variable "environment" {}
@@ -24,6 +28,12 @@ variable "environment" {}
 /**
  * Subnets
  */
+
+provider "aws" {
+    access_key = "${var.aws_access_key}"
+    secret_key = "${var.aws_secret_key}"
+    region     = "${var.aws_region}"
+}
 
 resource "aws_subnet" "main" {
   vpc_id                  = "${var.vpc_id}"
