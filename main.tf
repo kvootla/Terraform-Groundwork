7a5378b2f85c05798d1e7ed5762a1682f4624747
Krishna Vootla <vootla.krishna@gmail.com>
2017-04-17 08:53:44 -0400

    commit 34132

--- main.tf
+++ main.tf
@@ -10,25 +10,6 @@ variable "availability_zones" {
   description = "A list of availability zones inside the VPC"
 }
 
-variable "enable_dns_hostnames" {
-  description = "True to use private DNS within the VPC"
-  default     = true
-}
-
-variable "enable_dns_support" {
-  description = "True to use private DNS within the VPC"
-  default     = true
-}
-
-variable "private_propagating_vgws" {
-  description = "A list of VGWs the private route table should propagate."
-  default     = []
-}
-
-variable "private_subnets" {
-  default = "A list of public subnets inside the VPC"
-}
-
 variable "map_public_ip_on_launch" {
   description = "True to auto-assign public IP on launch"
   default     = true
