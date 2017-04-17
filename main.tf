64164970d7a1bafd8cc8b56216ddd81a807e442a
Krishna Vootla <vootla.krishna@gmail.com>
2017-04-17 10:22:15 -0400

    commit 34314

--- main.tf
+++ main.tf
@@ -63,7 +63,7 @@ resource "aws_route_table" "main" {
 resource "aws_route_table_association" "main" {
   subnet_id      = "${element(aws_subnet.main.*.id, count.index)}"
   route_table_id = "${element(aws_route_table.main.*.id, count.index)}"
-  count          = "${length(keys(var.public_subnets))}"
+  count          = "${length(keys(var.cidr_block ))}"
 
   lifecycle {
     create_before_destroy = true
