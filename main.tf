f6f24c67a5e9c2c6ffaec83ec6f7eee082d7c076
Krishna Vootla <vootla.krishna@gmail.com>
2017-04-23 21:41:24 -0400

    commit 32432

--- main.tf
+++ main.tf
@@ -21,20 +21,9 @@ variable "enable_dns_support" {
   default     = false
 }
 
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-variable "aws_region" {}
-
-
 /**
  * VPC
  */
-
-provider "aws" {
-    access_key = "${var.aws_access_key}"
-    secret_key = "${var.aws_secret_key}"
-    region = "${var.aws_region}"
-}
 
 //VPC Resource for Module
 resource "aws_vpc" "mod" {
