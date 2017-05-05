b72371d5e5995b641924775f3e5accc9e115415b
Krishna Vootla <vootla.krishna@gmail.com>
2017-05-05 10:16:34 -0400

    commit 33123

--- main.tf
+++ main.tf
@@ -11,9 +11,7 @@ variable "instance_type" {
   description = "Instance type, see a list at: https://aws.amazon.com/ec2/instance-types/"
 }
 
-variable "security_groups" {
-  description = "a comma separated lists of security group IDs"
-}
+variable "security_groups" { default = "" }
 
 variable "user_data" {
    description = "The path to a file with user_data for the instances"
