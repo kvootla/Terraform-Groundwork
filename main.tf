155a6d9d5484068ccc78acdc321de66291b2ee3d
Krishna Vootla <vootla.krishna@gmail.com>
2017-05-09 10:52:55 -0400

    commit 99809

--- main.tf
+++ main.tf
@@ -24,21 +24,11 @@ variable "environment" {
   default     = ""
 }
 
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-variable "aws_region" {}
-
 
 /**
  * Security Groups/SSH
  */
 
-provider "aws" {
-    access_key = "${var.aws_access_key}"
-    secret_key = "${var.aws_secret_key}"
-    region     = "${var.aws_region}"
-  }
-   
 resource "aws_security_group" "main_security_group" {
     name   = "${format("%s-%s-%s", var.organization, var.environment, var.sg_type)}"
     vpc_id = "${var.vpc_id}"
