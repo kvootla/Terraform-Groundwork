6607b70499befd85e02b909e060b8027f0bcda2b
Krishna Vootla <vootla.krishna@gmail.com>
2017-05-22 12:03:46 -0400

    commit 34322

--- main.tf
+++ main.tf
@@ -56,7 +56,7 @@ resource "aws_ebs_volume" "main" {
   }
 }
 
-resource "aws_volume_attachment" "ebs_attachment" {
+resource "aws_volume_attachment" "ebs_att" {
   device_name = "${var.device_name}"
   volume_id   = "${aws_ebs_volume.ebs.*.id}"
   instance_id = "${var.instance_id}"
