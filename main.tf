6f0bba83a301216d2cc39eac8e47f07c4f2b91a4
Krishna Vootla <vootla.krishna@gmail.com>
2017-06-01 13:31:14 -0400

    commit

--- main.tf
+++ main.tf
@@ -6,14 +6,6 @@ variable "name" {
   description = "Used to populate the name tag for the bucket"
 }
 
-variable "account_id" {
-  description = "Used to access into the bucket"
-}
-
-variable "user_name" {
-  description = "Used to access the bucket"
-}
-
 variable "environment" {
   description = "Environment tag, e.g prod"
 }
@@ -44,25 +36,4 @@ resource "aws_s3_bucket" "bucket" {
     Organization = "${var.organization}"
     Terraform    = "true"
   }
-  
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Id": "Policy1439656804052",
-    "Statement": [
-        {
-            "Sid": "Stmt1439656798784",
-            "Effect": "Allow",
-            "Principal": {
-                "AWS": "arn:aws:iam::"${var.account_id}":"${var.user_name}""
-            },
-            "Action": [
-                "s3:DeleteObject",
-                "s3:GetObject",
-                "s3:PutObject"
-            ],
-            "Resource": "arn:aws:s3:::${var.name}/*"
-        }
-    ]
-EOF
 }
