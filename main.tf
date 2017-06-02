f7c40d834f0adf7b32808bb19cc6226b3aadc989
kvootla <vootla.krishna@gmail.com>
2017-06-02 16:28:53 -0400

    initial commit

---
+++ main.tf
@@ -0,0 +1,36 @@
+/**
+* Inputs
+*/
+
+variable "allocation_id" {}
+
+variable "subnet_id" {}
+
+/**
+* NAT Gateway
+*/
+
+resource "aws_nat_gateway" "main" {
+  allocation_id = "${var.allocation_id}"
+  subnet_id     = "${var.subnet_id}"
+}
+
+/**
+* Outputs
+*/
+
+output "id" {
+  value = "${aws_nat_gateway.main.id}"
+}
+
+output "eni_id" {
+  value = "${aws_nat_gateway.main.network_interface_id}"
+}
+
+output "public_ip" {
+  value = "${aws_nat_gateway.main.public_ip}"
+}
+
+output "private_ip" {
+  value = "${aws_nat_gateway.main.private_ip}"
+}
