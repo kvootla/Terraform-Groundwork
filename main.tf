2810aa5ae3ec4c10ab2d0845238fedbb74e0836a
kvootla <vootla.krishna@gmail.com>
2017-06-07 16:53:10 -0400

    commit 43222

--- main.tf
+++ main.tf
@@ -2,22 +2,17 @@
  * Input Variables
  */
 
-variable "vpc" { default = true }
-variable "instance_ids" { default = "" }
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-variable "aws_region" {}
+variable "vpc" {
+  default = true
+}
 
+variable "instance_ids" {
+  default = ""
+}
 
 /**
  * Elastic IP
  */
-
-provider "aws" {
-    access_key = "${var.aws_access_key}"
-    secret_key = "${var.aws_secret_key}"
-    region = "${var.aws_region}"
-}
 
 resource "aws_eip" "eip" {
   instance = "${element(split(",", var.instance_ids), count.index)}"
@@ -25,9 +20,10 @@ resource "aws_eip" "eip" {
   count    = "${length(split(",", var.instance_ids))}"
 }
 
-
 /**
  * Outputs Varibales
  */
 
-output "public_ip" { value = "${aws_eip.eip.public_ip}" }
+output "public_ip" {
+  value = "${aws_eip.eip.public_ip}"
+}
