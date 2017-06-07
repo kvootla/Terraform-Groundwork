520eefbb303402f0696969b0fddde2553cd53801
kvootla <vootla.krishna@gmail.com>
2017-06-07 16:45:01 -0400

    commit 41221

--- main.tf
+++ main.tf
@@ -1,4 +1,3 @@
-
 /**
  * Inputs
  */
@@ -25,31 +24,28 @@ variable "environment" {
   default     = ""
 }
 
-
 /**
  * Security Groups/Oracle
  */
-  
+
 resource "aws_security_group" "main_security_group" {
-    name   = "${format("%s-%s-%s", var.organization, var.environment, var.sg_type)}"
-    vpc_id = "${var.vpc_id}"
-  
+  name   = "${format("%s-%s-%s", var.organization, var.environment, var.sg_type)}"
+  vpc_id = "${var.vpc_id}"
+
   // allow traffic for TCP 1521
-    ingress {
-        from_port = 1521
-        to_port = 1521
-        protocol = "tcp"
-        cidr_blocks = ["${var.source_cidr_block}"]
-    }
-      
+  ingress {
+    from_port   = 1521
+    to_port     = 1521
+    protocol    = "tcp"
+    cidr_blocks = ["${var.source_cidr_block}"]
+  }
+
   tags {
     Name         = "${format("%s-%s-%s", var.organization, var.environment, var.sg_type)}-sg"
     Organization = "${var.organization}"
     Terraform    = "true"
   }
-    
 }
-
 
 /**
  * Outputs
