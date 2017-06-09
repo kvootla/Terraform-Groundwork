5aa8b74f110c03bacfceef30b3eee1e036b37f99
kvootla <vootla.krishna@gmail.com>
2017-06-09 11:23:21 -0400

    commit 23214

--- main.tf
+++ main.tf
@@ -8,6 +8,10 @@ variable "name" {
 
 variable "vpc_id" {}
 
+variable "subnet_id" {
+  description = "The VPC subnet the instance(s) will go in"
+}
+
 variable "environment" {
   description = "Environment tag, e.g prod"
 }
@@ -18,6 +22,7 @@ variable "environment" {
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = "${var.vpc_id}"
+  subnet_id      = "${var.subnet_id}"
 
   tags {
     Name        = "${var.name}"
