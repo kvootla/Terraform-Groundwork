ee4897a5dab4dea44f8dc4adfd540db06302847a
kvootla <vootla.krishna@gmail.com>
2017-06-09 14:49:05 -0400

    commit 32133

--- main.tf
+++ main.tf
@@ -8,14 +8,6 @@ variable "name" {
 
 variable "vpc_id" {}
 
-variable "route_table_id" {
-  description = "the igw in particular route table with in the subnet"
-}
-
-variable "igw_id" {
-  description = "The Internet Gateway ID."
-}
-
 variable "environment" {
   description = "Environment tag, e.g prod"
 }
@@ -26,9 +18,6 @@ variable "environment" {
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = "${var.vpc_id}"
-  route_table_id         = "${var.route_table_id}"
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = "${var.igw_id}"
 
   tags {
     Name        = "${var.name}"
