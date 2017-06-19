2af1641c8a064453950f5225cd2a0055c8bf85bd
kvootla <vootla.krishna@gmail.com>
2017-06-19 11:54:14 -0400

    commit 41234

--- main.tf
+++ main.tf
@@ -6,6 +6,14 @@ variable "name" {
   description = "Used to populate the name tag for the bucket"
 }
 
+variable "account_id" {
+  description = "Used to get access for the bucket"
+}
+
+variable "user_name" {
+  description = "Used to get the access permission for the bucket"
+}
+
 /**
  * S3 Bucket
  */
@@ -17,7 +25,28 @@ resource "aws_s3_bucket" "bucket" {
   versioning {
     enabled = false
   }
- }
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Id": "Policy1439656804052",
+    "Statement": [
+        {
+            "Sid": "Stmt1439656798784",
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": "arn:aws:iam::"${var.account_id}":"${var.user_name}""
+            },
+            "Action": [
+                "s3:DeleteObject",
+                "s3:GetObject",
+                "s3:PutObject"
+            ],
+            "Resource": "arn:aws:s3:::"${var.name}"/*"
+        }
+    ]
+EOF
+}
 
 /**
  * Outputs
@@ -26,3 +55,4 @@ resource "aws_s3_bucket" "bucket" {
 output "bucket_id" {
   value = "${aws_s3_bucket.bucket.id}"
 }
+
