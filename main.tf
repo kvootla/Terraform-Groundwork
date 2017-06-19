8b0e529581ec738e19780124888237739595348c
kvootla <vootla.krishna@gmail.com>
2017-06-19 11:36:18 -0400

    commit 13248

--- main.tf
+++ main.tf
@@ -6,14 +6,6 @@ variable "name" {
   description = "Used to populate the name tag for the bucket"
 }
 
-variable "account_id" {
-  description = "Used to get access for the bucket"
-}
-
-variable "user_name" {
-  description = "Used to get the access permission for the bucket"
-}
-
 /**
  * S3 Bucket
  */
