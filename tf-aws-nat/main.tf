fd5daf5ee11727650135b786bb0a906a6b235c4b
kvootla <vootla.krishna@gmail.com>
2017-08-02 13:31:44 -0400

    commit 34385

---
+++ tf-aws-nat/main.tf
@@ -0,0 +1,25 @@
+/**
+* Inputs
+*/
+
+variable "private_subnet_id" {}
+
+/**
+ * Nat
+ */
+resource "aws_eip" "main" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "main" {
+  allocation_id = "${aws_eip.main.id}"
+  subnet_id     = "${var.public_subnet_id}"
+}
+
+/**
+ * Outputs
+ */
+
+output "nat_id" {
+  value = "${aws_nat_gateway.main.id}"
+}
