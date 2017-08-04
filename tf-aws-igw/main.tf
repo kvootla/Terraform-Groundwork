fbbaa77c2d3964f6d8b5f3b2f156196d39927ef6
kvootla <vootla.krishna@gmail.com>
2017-08-04 11:19:01 -0400

    commit 25450

--- tf-aws-igw/main.tf
+++ tf-aws-igw/main.tf
@@ -4,10 +4,6 @@
 
 variable "vpc_id" {}
 
-variable "application" {
-  description = "Application that will use the cache"
-}
-
 variable "organization" {
   description = "Organization the VPC is for."
 }
@@ -17,20 +13,16 @@ variable "environment" {
   default     = ""
 }
 
-variable "environment" {
-  description = "Environment tag, e.g prod"
-}
-
 /**
  * Internet Gateway
  */
 
 resource "aws_internet_gateway" "main" {
   vpc_id = "${var.vpc_id}"
-  name   = "${var.organization}-${var.environment}-${var.application}-igw"
+  name   = "${var.organization}-${var.environment}-igw"
 
   tags {
-    Name         = "${format("%s-%s-%s", var.organization, var.environment, var.application)}-igw"
+    Name         = "${format("%s-%s-%s", var.organization, var.environment)}-igw"
     Organization = "${var.organization}"
     Terraform    = "true"
   }
