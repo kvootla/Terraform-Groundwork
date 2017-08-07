62275dfb6744a3aa17ed2da46d3699b20d4f561b
kvootla <vootla.krishna@gmail.com>
2017-08-07 10:16:00 -0400

    commit 43109

--- tf-aws-elb/web/main.tf
+++ tf-aws-elb/web/main.tf
@@ -15,20 +15,78 @@ variable "subnet_group_a2" {
   description = "The subnet for availability zone 2"
 }
 
+variable "instance_ids" {
+  description = "List of instance IDs"
+  default = []
+}
+
 variable "security_group_id" {
   description = "List of security group IDs"
 }
 
-variable "backend_port" {
-  description = "Instance port"
+variable "connection_draining" {
+  description = "Enable connection draining"
+  default     = true
 }
 
-variable "backend_protocol" {
-  description = "Protocol to use, HTTP or TCP"
+variable "connection_draining_timeout" {
+  description = "Enable connection draining timeout period (in seconds)"
+  default     = 300
+}
+
+variable "idle_timeout" {
+  description = "Idle timeout period (in seconds)"
+  default     = 60
+}
+
+variable "cross_zone_load_balancing" {
+  description = "Enable cross zone load balancing"
+  default     = true
+}
+
+variable "http_backend_port" {
+  description = "Backend instance port for the http listener"
+  default     = 80
+}
+
+variable "http_backend_protocol" {
+  description = "Backend protocol for the http listener"
+  default     = "http"
+}
+
+variable "https_backend_port" {
+  description = "Backend instance port for the https listener"
+  default     = 443
+}
+
+variable "https_backend_protocol" {
+  description = "Backend protocol for the https listener"
+  default     = "https"
+}
+
+variable "healthy_threshold" {
+  description = "Healthy threshold"
+  default     = 2
+}
+
+variable "unhealthy_threshold" {
+  description = "Unhealthy threshold"
+  default     = 2
+}
+
+variable "health_check_timeout" {
+  description = "Healthcheck timeout (in seconds)"
+  default     = 5
+}
+
+variable "health_check_interval" {
+  description = "Healthcheck interval (in seconds)"
+  default     = 10
 }
 
 variable "health_check_target" {
   description = "Healthcheck path"
+  default     = "HTTPS:443/"
 }
 
 variable "application" {
@@ -52,12 +110,13 @@ resource "aws_elb" "main_elb" {
   name               = "${var.organization}-${var.environment}-${var.application}-elb"
   internal           = "${var.internal}"
   subnet_ids         = ["${var.subnet_ids}"]
+  instances          = ["${var.instance_ids}"]
   security_group_ids = ["${var.security_group_ids}"]
 
-  idle_timeout                = 30
-  connection_draining         = true
-  connection_draining_timeout = 15
-  cross_zone_load_balancing   = true
+  idle_timeout                = "${var.idle_timeout}"
+  connection_draining         = "${var.connection_draining}"
+  connection_draining_timeout = "${var.connection_draining_timeout}"
+  cross_zone_load_balancing   = "${var.cross_zone_load_balancing}"
 
   listener {
     lb_port           = 80
@@ -74,15 +133,15 @@ resource "aws_elb" "main_elb" {
   }
 
   health_check {
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-    timeout             = 3
+    healthy_threshold   = "${var.healthy_threshold}"
+    unhealthy_threshold = "${var.unhealthy_threshold}"
+    timeout             = "${var.health_check_timeout}"
     target              = "${var.health_check_target}"
-    interval            = 30
+    interval            = "${var.health_check_interval}"
   }
 
   tags {
-    Name         = "${format("%s-%s-%s", var.organization, var.environment, var.application)}-i"
+    Name         = "${format("%s-%s-%s", var.organization, var.environment, var.application)}-elb"
     Organization = "${var.organization}"
     Terraform    = "true"
   }
