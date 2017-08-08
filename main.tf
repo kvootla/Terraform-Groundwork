bb4d4777f8dd7207dd6d903bfdcb8cc8113d3bda
kvootla <vootla.krishna@gmail.com>
2017-08-08 10:16:24 -0400

    commit 31153

---
+++ main.tf
@@ -0,0 +1,39 @@
+/**
+ * Inputs
+ */
+
+variable "application" {
+  description = "Application that will use the cache"
+}
+
+variable "organization" {
+  description = "Organization the VPC is for."
+}
+
+variable "environment" {
+  description = "Environment the VPC is for."
+  default     = ""
+}
+
+/**
+ * Simple Notification Services/Topic
+ */
+
+resource "aws_sns_topic" "main" {
+   name            = "sns-${var.organization}-${var.environment}-${var.application}"
+
+   tags {
+    Name         = "${format("%s-%s-%s", var.organization, var.environment, var.application)}-elb"
+    Organization = "${var.organization}"
+    Terraform    = "true"
+  }
+}
+
+/**
+ * Outputs
+ */
+
+output "sns_id" {
+    value = "${aws_sns_topic.main.id}"
+}
+
