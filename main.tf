55e05dd15187653ae7c44afc2557500d6f9dafc3
kvootla <vootla.krishna@gmail.com>
2017-08-08 12:03:33 -0400

    commit 31453

---
+++ main.tf
@@ -0,0 +1,37 @@
+/**
+ * Inputs
+ */
+
+variable "topic_arn" {
+  description = "The ARN of the SNS topic to subscribe"
+}
+
+variable "protocol" {
+  description = "The possible values for protocol are sqs, lambda, application"
+}
+
+variable "endpoint" {
+  description = "The endpoint to send data to, the contents will vary with the protocol"
+}
+
+/**
+ * Simple Notification Services/Topic Subscription
+ */
+
+resource "aws_sns_topic_subscription" "main" {
+  topic_arn = "${var.topic_arn}"
+  protocol  = "${var.protocol}"
+  endpoint  = "${var.endpoint}"
+}
+
+/**
+ * Outputs
+ */
+
+output "sns_id" {
+  value = "${aws_sns_topic.main.id}"
+}
+
+output "arn" {
+  value = "${aws_sns_topic_subscription.main.outputs.ARN}"
+}
