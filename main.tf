bd066284dfe07c01f56660c3884e5c9d53c1866a
kvootla <vootla.krishna@gmail.com>
2017-08-09 11:02:59 -0400

    commit 24809

--- main.tf
+++ main.tf
@@ -2,17 +2,9 @@
  * Input Variables
  */
 
-variable "alb_is_internal" {
+variable "internal" {
   description = "Determines if the ALB is internal. Default: false"
   default     = false
-}
-
-variable "alb_name" {
-  description = "The name of the ALB as will show in the AWS EC2 ELB console."
-}
-
-variable "alb_security_groups" {
-  description = "A comma separated string of sg's with which we associate the ALB. e.g. 'sg-edcd9784,sg-edcd9785'"
 }
 
 variable "log_bucket" {
@@ -23,8 +15,25 @@ variable "log_prefix" {
   description = "S3 prefix within the log_bucket under which logs are stored."
 }
 
-variable "subnets" {
-  description = "A comma delimited list of subnets to associate with the ALB. e.g. 'subnet-1a2b3c4d,subnet-1a2b3c4e,subnet-1a2b3c4f'"
+variable "security_group_ids" {
+  description = "A comma separated string of sg's with which we associate the ALB."
+}
+
+variable "subnet_ids" {
+  description = "A comma delimited list of subnets to associate with the ALB."
+}
+
+variable "application" {
+  description = "Application that will use the cache"
+}
+
+variable "organization" {
+  description = "Organization the ALB is for."
+}
+
+variable "environment" {
+  description = "Environment the ALB is for."
+  default     = ""
 }
 
 /**
@@ -32,10 +41,10 @@ variable "subnets" {
  */
 
 resource "aws_alb" "alb_loging" {
-  name            = "${var.alb_name}"
-  subnets         = ["${split(",", var.subnets)}"]
-  security_groups = ["${split(",", var.alb_security_groups)}"]
-  internal        = "${var.alb_is_internal}"
+  name               = "elb-${var.organization}-${var.environment}-${var.application}"
+  subnet_ids         = ["${split(",", var.subnet_ids)}"]
+  security_group_ids = ["${split(",", var.security_group_ids)}"]
+  internal           = "${var.internal}"
 
   access_logs {
     bucket = "${var.log_bucket}"
@@ -46,10 +55,10 @@ resource "aws_alb" "alb_loging" {
 }
 
 resource "aws_alb" "alb_nologing" {
-  name            = "${var.alb_name}"
-  subnets         = ["${split(",", var.subnets)}"]
-  security_groups = ["${split(",", var.alb_security_groups)}"]
-  internal        = "${var.alb_is_internal}"
+  name               = "elb-${var.organization}-${var.environment}-${var.application}"
+  subnet_ids         = ["${split(",", var.subnet_ids)}"]
+  security_group_ids = ["${split(",", var.security_group_ids)}"]
+  internal           = "${var.internal}"
 
   count = "${(var.log_bucket == "" || var.log_prefix == "") ? 1 : 0}"
 }
