4d901b61a327ee582b96fc5a2b443eee0f53c133
kvootla <vootla.krishna@gmail.com>
2017-08-09 10:56:06 -0400

    commit 34255

--- main.tf
+++ main.tf
@@ -60,18 +60,12 @@ resource "aws_alb" "alb_nologing" {
 
 output "dns_name" {
   value = "${var.alb_loging.dns_name}"
-
-  /*value = "${coalesce(list("var.alb_loging.dns_name", "var.alb_nologing.dns_name")) }"*/
 }
 
 output "id" {
   value = "${var.alb_loging.id}"
-
-  /*value = "${coalesce(list("var.alb_loging.dns_name", "var.alb_nologing.dns_name")) }"*/
 }
 
 output "zone_id" {
   value = "${var.alb_loging.zone_id}"
-
-  /*value = "${coalesce(list("var.alb_loging.dns_name", "var.alb_nologing.dns_name")) }"*/
 }
