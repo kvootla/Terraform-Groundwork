7f8a7fe98ab2959a89081a66bb2dc88e5538eba1
kvootla <vootla.krishna@gmail.com>
2017-08-10 13:52:50 -0400

    commit 23453

---
+++ main.tf
@@ -0,0 +1,49 @@
+/*
+ * Inputs
+ */
+
+variable "arn" {}
+variable "instance_id" {}
+
+
+/**
+ * CloudWatch Event Target
+ */
+
+resource "aws_cloudwatch_event_target" "main" {
+  target_id = "main"
+  rule      = "${aws_cloudwatch_event_rule.console.name}"
+  arn       = "${var.arn}"
+
+  run_command_targets {
+    key = "InstanceIds"
+    values = "${var.instance_id}"
+  }
+}
+
+resource "aws_cloudwatch_event_rule" "console" {
+  name        = "capture-ec2-scaling-events"
+  description = "Capture all EC2 scaling events"
+
+  event_pattern = <<PATTERN
+{
+  "source": [
+    "aws.autoscaling"
+  ],
+  "detail-type": [
+    "EC2 Instance Launch Successful",
+    "EC2 Instance Terminate Successful",
+    "EC2 Instance Launch Unsuccessful",
+    "EC2 Instance Terminate Unsuccessful"
+  ]
+}
+PATTERN
+}
+
+/**
+ * Outputs
+ */
+
+ output "cloudwatch_id" {
+  value = "${aws_cloudwatch_event_target.main.id}"
+}
