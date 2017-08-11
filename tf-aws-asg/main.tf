2b5b89e19235bb8d83834094a55d0cb07a984011
kvootla <vootla.krishna@gmail.com>
2017-08-11 14:25:32 -0400

    commit 34545

--- tf-aws-asg/main.tf
+++ tf-aws-asg/main.tf
@@ -2,12 +2,11 @@
  * Input Variables
  */
 
-variable "lc_name" {}
+variable "config_name" {}
 variable "ami_id" {}
 variable "key_name" {}
 variable "instance_type" {}
 variable "iam_instance_profile" {}
-variable "subnet_group_azs" {}
 
 variable "health_check_type" {
   default = "EC2"
@@ -17,24 +16,28 @@ variable "azs" {
   description = "Availability Zones"
 }
 
-variable "security_group" {
+variable "security_group_ids" {
   description = "The security group the instances to use"
 }
+
+variable "subnet_group_ids" {
+   description = "The subnet IDs with in the VPC"
+ }
 
 variable "user_data" {
   description = "The path to a file with user_data for the instances"
 }
 
-variable "asg_maximum_number_of_instances" {
+variable "max_number_of_instances" {
   description = "The maximum number of instances in the ASG should maintain"
 }
 
-variable "asg_minimum_number_of_instances" {
+variable "min_number_of_instances" {
   description = "The minimum number of instances in the ASG should maintain"
   default     = 2
 }
 
-variable "health_check_grace_period" {
+variable "health_check_period" {
   description = "Number of seconds for a health check to time out"
   default     = 300
 }
@@ -57,7 +60,7 @@ variable "environment" {
  */
 
 resource "aws_launch_configuration" "launch_config" {
-  name                 = "${var.organization}-${var.environment}-${var.application}-{var.lc_name}-elb"
+  name                 = "${var.organization}-${var.environment}-${var.application}-{var.config_name}-asg"
   image_id             = "${var.ami_id}"
   instance_type        = "${var.instance_type}"
   iam_instance_profile = "${var.iam_instance_profile}"
@@ -66,17 +69,17 @@ resource "aws_launch_configuration" "launch_config" {
   user_data            = "${file(var.user_data)}"
 }
 
-resource "aws_autoscaling_group" "asg" {
+resource "aws_autoscaling_group" "main" {
   depends_on           = ["aws_launch_configuration.launch_config"]
-  name                 = "${var.organization}-${var.environment}-${var.application}-elb"
+  name                 = "${var.organization}-${var.environment}-${var.application}-asg"
   availability_zones   = ["${split(",", var.azs)}"]
-  vpc_zone_identifier  = ["${split(",", var.subnet_group_azs)}"]
+  vpc_zone_identifier  = ["${var.subnet_group_ids}"]
   launch_configuration = "${aws_launch_configuration.launch_config.id}"
 
-  max_size                  = "${var.asg_maximum_number_of_instances}"
-  min_size                  = "${var.asg_minimum_number_of_instances}"
-  desired_capacity          = "${var.asg_maximum_number_of_instances}"
-  health_check_grace_period = "${var.health_check_grace_period}"
+  max_size                  = "${var.max_number_of_instances}"
+  min_size                  = "${var.min_number_of_instances}"
+  desired_capacity          = "${var.max_number_of_instances}"
+  health_check_period = "${var.health_check_period}"
   health_check_type         = "${var.health_check_type}"
 }
 
@@ -89,5 +92,5 @@ output "launch_config_id" {
 }
 
 output "asg_id" {
-  value = "${aws_autoscaling_group.asg.id}"
+  value = "${aws_autoscaling_group.main.id}"
 }
