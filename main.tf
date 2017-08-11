1a514eea69dd81f1e2677848c33435b5b60f1c8c
kvootla <vootla.krishna@gmail.com>
2017-08-11 09:30:09 -0400

    commit 42552

--- main.tf
+++ main.tf
@@ -2,25 +2,49 @@
  * Inputs
  */
 
-variable "region" {}
-variable "account_id" {}
-variable "ebs_vol_a_id" {}
+variable "region" {
+description = "The region of the EBS volume to be snapshot"
+  default     = "us-east-1"
+}
+variable "account_id" {
+ description = "The canonical user ID associated with the aws account"
+}
+
+variable "ebs_volume_id" {
+  description = "EBS volume ID for capturing the snapshot"
+}
+variable "cron_expression" {
+  description = "Scheduling expression to capture snapshot at certain interval"
+  default = "cron(0 20 * * ? *)" 
+}
+
+variable "application" {
+  description = "Application that will use the cache"
+}
+
+variable "organization" {
+  description = "Organization the VPC is for."
+}
+
+variable "environment" {
+  description = "Environment the VPC is for."
+  default     = ""
+}
 
 /**
  * CloudWatch Event Target
  */
 
 resource "aws_cloudwatch_event_target" "main" {
-  target_id = "ebs_vol_a"
+  target_id = "main"
   rule      = "${aws_cloudwatch_event_rule.snap_ebs.name}"
-  arn       = "arn:aws:automation:${var.region}:${var.account_id}:action/EBSCreateSnapshot/EBSCreateSnapshot_ebs_vol_a"
-  input     = "\"arn:aws:ec2:${var.region}:${var.account_id}:volume/vol-${var.ebs_vol_a_id}\""
+  arn       = "arn:aws:automation:${var.region}:${var.account_id}:action/EBSCreateSnapshot/EBSCreateSnapshot_ebs_volume"
+  input     = "\"arn:aws:ec2:${var.region}:${var.account_id}:volume/vol-${var.ebs_volume_id}\""
 }
 
 resource "aws_cloudwatch_event_rule" "snap_ebs" {
-  name                = "snap-ebs-volumes"
-  description         = "Snapshot EBS volumes"
-  schedule_expression = "cron(0 20 * * ? *)"
+  name                = "${var.organization}-${var.environment}-${var.application}-ebs"
+  schedule_expression = "${var.cron_expression}"
 }
 
 /**
