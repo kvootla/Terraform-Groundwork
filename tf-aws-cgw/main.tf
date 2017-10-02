e99ceb81aaca2563414cf52269474da3a9387ada
kvootla <vootla.krishna@gmail.com>
2017-10-02 17:48:29 +0000

    commit 27801

---
+++ tf-aws-cgw/main.tf
@@ -0,0 +1,61 @@
+/*
+ * Inputs
+ */
+
+variable "customer_gateway_id" {
+  description = "customer gateway id"
+  default     = true
+}
+
+variable "ip_address" {
+  description = "IP address of the Customer Gateway external interface."
+}
+
+variable "bgp_asn" {
+  description = "BGP ASN of the Customer Gateway. By convention, use 65000 if you are not running BGP."
+}
+
+variable "organization" {
+  description = "Organization the VPC is for."
+}
+
+variable "environment" {
+  description = "Environment the VPC is for."
+  default     = ""
+}
+
+/**
+ * Customer Gateways
+ */
+
+resource "aws_customer_gateway" "main" {
+  bgp_asn    = "${var.bgp_asn}"
+  ip_address = "${var.ip_address}"
+  type       = "ipsec.1"
+
+  tags {
+    Name         = "${var.environment == "" ? var.organization : format("%s-%s", var.organization, var.environment)}-VPN"
+    Organization = "${var.organization}"
+    Terraform    = "true"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+/**
+ * Outputs
+ */
+
+output "cgw_id" {
+  value = "${aws_customer_gateway.main.id}"
+}
+
+output "cgw_ip_address" {
+  value = "${aws_customer_gateway.main.ip_address}"
+}
+
+output "cgw_bgp_asn" {
+  value = "${aws_customer_gateway.main.bgp_asn}"
+}
