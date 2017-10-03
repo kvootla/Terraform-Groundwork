bfe953e27e96524a60950905b17d85f5f396ca60
kvootla <vootla.krishna@gmail.com>
2017-10-03 16:59:26 +0000

    commit 98115

--- tf-aws-snapshot/main.tf
+++ tf-aws-snapshot/main.tf
@@ -22,9 +22,10 @@ variable "environment" {
  */
 
 resource "aws_ami_from_instance" "main" {
+  name                    = "${var.organization}-${var.environment}-${var.application}-ami-snapshot"
   source_instance_id      = "${var.source_instance_id}"
   snapshot_without_reboot = "${var.snapshot_without_reboot}"
-  
+
   tags {
     Name         = "${var.environment == "" ? var.organization : format("%s-%s", var.organization, var.environment)}-ami"
     Organization = "${var.organization}"
