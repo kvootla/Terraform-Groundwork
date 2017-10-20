289df2fb417d0107e43a08cde8a3c541b665f8f9
Krishna Vootla <vootla.krishna@gmail.com>
2017-10-20 15:35:58 -0400

    commit 42091

--- main.tf
+++ main.tf
@@ -23,6 +23,7 @@ variable "route_table_ids" {
 
 resource "aws_vpc_endpoint" "s3" {
   vpc_id          = "${var.vpc_id}"
+  service_name    = "${var.service_name}"
   route_table_ids = ["${var.route_table_ids}"]
 }
 
