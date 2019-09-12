4c24c36fd5ca01729c02ea045e1dfacd1d6da643
kvootla <vootla.krishna@gmail.com>
2019-09-12 18:58:46 +0000

    removed the outputs which were commented and formatted

--- tf-aws-spot-instance/main.tf
+++ tf-aws-spot-instance/main.tf
@@ -245,26 +245,6 @@ output "id" {
   value       = ["${aws_spot_instance_request.main.*.id}"]
 }
 
-#output "public_dns" {
-#  description = "List of public DNS names assigned to the instances. For EC2-VPC, this is only available if you've enabled DNS hostnames for your VPC"
-#  value       = ["${aws_spot_instance_request.main.*.public_dns}"]
-#}
-
-#output "public_ip" {
-#  description = "List of public IP addresses assigned to the instances, if applicable"
-#  value       = ["${aws_spot_instance_request.main.*.public_ip}"]
-#}
-
-#output "private_dns" {
-#  description = "List of private DNS names assigned to the instances. Can only be used inside the Amazon EC2, and only available if you've enabled DNS hostnames for your VPC"
-#  value       = ["${aws_spot_instance_request.main.*.private_dns}"]
-#}
-
-#output "private_ip" {
-#  description = "List of private IP addresses assigned to the instances"
-#  value       = ["${aws_spot_instance_request.main.*.private_ip}"]
-#}
-
 output "spot_bid_status" {
   description = "The current bid status of the Spot Instance Request."
   value       = ["${aws_spot_instance_request.main.*.spot_bid_status}"]
@@ -274,8 +254,3 @@ output "spot_request_state" {
   description = "The current request state of the Spot Instance Request."
   value       = ["${aws_spot_instance_request.main.*.spot_request_state}"]
 }
-
-#output "spot_instance_id" {
-#  description = "The Instance ID (if any) that is currently fulfilling the Spot Instance request."
-#  value       = ["${aws_spot_instance_request.main.*.spot_instance_id}"]
-#}
